c690fb924e905794750d7b1c1b2cc0069fb8940d
Dibya Dahal <102875464+Killerbee7@users.noreply.github.com>
2022-10-19 16:31:00 +0300

    2 players dice game

--- Dicee Challenge - Starting Files/main.js
+++ Dicee Challenge - Starting Files/main.js
@@ -1,79 +1,45 @@
-const p1=document.querySelector(".img1");
-const p2=document.querySelector('.img2');
-const h1=document.querySelector('h1');
+const p1 = document.querySelector(".img1");
+const p2 = document.querySelector(".img2");
+const h1 = document.querySelector("h1");
 
+const randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-    const randomNumber1=Math.floor(Math.random()*6)+1;
+console.log(randomNumber1);
 
-    console.log(randomNumber1);
-
-    if (randomNumber1===1){
-        p1.setAttribute('src', 'images/dice1.png')
-    }
-
-    else if (randomNumber1===2){
-        p1.setAttribute('src', 'images/dice2.png')
-    }
-
-    else if (randomNumber1===3){
-        p1.setAttribute('src', 'images/dice3.png')
-    }
-
-    else if (randomNumber1===4){
-        p1.setAttribute('src', 'images/dice4.png')
-    }
-
-    else if (randomNumber1===5){
-        p1.setAttribute('src', 'images/dice5.png')
-    }
-
-    else{
-        p1.setAttribute('src', 'images/dice6.png')
-    };
-
-
-
-    const randomNumber2=Math.floor(Math.random()*6)+1;
-
-    console.log(randomNumber2);
-
-    if (randomNumber2===1){
-        p2.setAttribute('src', 'images/dice1.png')
-    }
-
-    else if (randomNumber2===2){
-        p2.setAttribute('src', 'images/dice2.png')
-    }
-
-    else if (randomNumber2===3){
-        p2.setAttribute('src', 'images/dice3.png')
-    }
-
-    else if (randomNumber2===4){
-        p2.setAttribute('src', 'images/dice4.png')
-    }
-
-    else if (randomNumber2===5){
-        p2.setAttribute('src', 'images/dice5.png')
-    }
-
-    else{
-        p2.setAttribute('src', 'images/dice6.png')
-    };
-
-
-
-
-
-
-if(randomNumber1>randomNumber2){
-
-   h1.innerHTML= "player1 wins";
-
+if (randomNumber1 === 1) {
+  p1.setAttribute("src", "images/dice1.png");
+} else if (randomNumber1 === 2) {
+  p1.setAttribute("src", "images/dice2.png");
+} else if (randomNumber1 === 3) {
+  p1.setAttribute("src", "images/dice3.png");
+} else if (randomNumber1 === 4) {
+  p1.setAttribute("src", "images/dice4.png");
+} else if (randomNumber1 === 5) {
+  p1.setAttribute("src", "images/dice5.png");
+} else {
+  p1.setAttribute("src", "images/dice6.png");
 }
 
-else if(randomNumber1<randomNumber2){
+const randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-    h1.innerHTML= "player2 wins";
+console.log(randomNumber2);
 
-};
+if (randomNumber2 === 1) {
+  p2.setAttribute("src", "images/dice1.png");
+} else if (randomNumber2 === 2) {
+  p2.setAttribute("src", "images/dice2.png");
+} else if (randomNumber2 === 3) {
+  p2.setAttribute("src", "images/dice3.png");
+} else if (randomNumber2 === 4) {
+  p2.setAttribute("src", "images/dice4.png");
+} else if (randomNumber2 === 5) {
+  p2.setAttribute("src", "images/dice5.png");
+} else {
+  p2.setAttribute("src", "images/dice6.png");
+}
+
+if (randomNumber1 > randomNumber2) {
+  h1.innerHTML = "player1 wins";
+} else if (randomNumber1 < randomNumber2) {
+  h1.innerHTML = "player2 wins ";
+}

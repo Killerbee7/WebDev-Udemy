c62308506a89a19eac8ffd5c3598f7ceb3169740
Dibya Dahal <102875464+Killerbee7@users.noreply.github.com>
2022-10-19 16:34:16 +0300

    drum kit working

---
+++ Drum Kit Starting Files/calc.js
@@ -0,0 +1,23 @@
+function add(a, b){
+
+    return a+b;
+}
+
+function substract(a, b){
+
+    return a-b;
+}
+
+function multiply(a, b){
+
+    return a*b;
+}
+
+function divide(a, b){
+
+    return a/b;
+}
+
+function result(a, b, operator){
+    return operator(a,b)
+};

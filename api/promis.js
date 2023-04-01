bc533c3135af30505de1d928a04a7066e0c79dcb
Dibya Dahal <102875464+Killerbee7@users.noreply.github.com>
2023-04-02 00:49:38 +0300

    projects added

--- api/promis.js
+++
@@ -1,9 +0,0 @@
-let p= new Promise(( resolve,reject) => { 
-    console.log("promise is pending");
-    setTimeout(() =>{
-        console.log("i am a promise and i am fullfiulled")
-        resolve(true)}, 5000)
-    
-})
-
-console.log(p);

e5058779c39cc36b88d452710a3dfe7bf72e5d33
Dibya Dahal <102875464+Killerbee7@users.noreply.github.com>
2023-04-05 23:37:31 +0300

    server port changed

---
+++ my-express-server/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express()
+
+app.get("/", (request, response) =>{
+    response.send("<h1>Hello Guys. This is Udemy Node</h1>")
+});
+
+app.listen(3000,()=>{
+    console.log("server started on port 3000");
+});

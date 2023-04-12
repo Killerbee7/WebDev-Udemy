28e9f0bca001cba243e0c11e85ad6749ef85a8a9
Dibya Dahal <102875464+Killerbee7@users.noreply.github.com>
2023-04-12 23:39:11 +0300

    server updated

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -2,10 +2,16 @@ const express = require("express");
 
 const app = express()
 
+
 app.get("/", (request, response) =>{
     response.send("<h1>Hello Guys. This is Udemy Node</h1>")
 });
 
-app.listen(3005,()=>{
-    console.log("server started on port 3005");
+app.listen(3006,()=>{
+    console.log("server started on port 3006");
 });
+
+
+function authenticationDn(username, password){
+
+}
